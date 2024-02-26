eddc7f43f2703d17981408384ff6aec14ddb29aa
Jocelyn AGORO <61328035+Jocelyn-AGORO@users.noreply.github.com>
2024-02-26 15:39:25 +0100

    retrieve some code quality errors

--- python/tennis1.py
+++ python/tennis1.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
-
+# Frontière metier/technique floue
 class TennisGame1:
 
     def __init__(self, player1Name, player2Name):
         self.player1Name = player1Name
         self.player2Name = player2Name
-        self.p1points = 0
-        self.p2points = 0
+        self.p1points = 0 #TODO: Convention de nommage
+        self.p2points = 0 #TODO: Convention de nommage
 
     def won_point(self, playerName):
         if playerName == "player1":
             self.p1points += 1
         else:
             self.p2points += 1
-
+    #TODO: God Function    
     def score(self):
         result = ""
-        tempScore=0
+        tempScore=0 #TODO: Scope trop global, n'est pas utilisé dans le bloque if
         if (self.p1points==self.p2points):
             result = {
                 0 : "Love-All",
@@ -27,15 +27,15 @@ class TennisGame1:
             minusResult = self.p1points-self.p2points
             if (minusResult==1):
                 result ="Advantage player1"
-            elif (minusResult ==-1):
+            elif (minusResult ==-1): #TODO: Condition magiques
                 result ="Advantage player2"
-            elif (minusResult>=2):
+            elif (minusResult>=2): #TODO: Condition magiques
                 result = "Win for player1"
             else:
                 result ="Win for player2"
         else:
             for i in range(1,3):
-                if (i==1):
+                if (i==1): #TODO: Control structure depth
                     tempScore = self.p1points
                 else:
                     result+="-"
